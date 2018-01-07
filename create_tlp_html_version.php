3d6a5d9aa8aba373c3b98e3643dc0042d5cf4bb2
Kevin Klement <klement@umass.edu>
2018-01-07 11:15:29 -0500

    kckupdate push 1515341729

--- create_tlp_html_version.php
+++ create_tlp_html_version.php
@@ -9,6 +9,16 @@ if (!(isset($tlp->{'P1'}))) {
     exit_with_error('Could not parse HTML-based json file.');
 }
 
+// process settings
+$columns_mode = true;
+$settings_str = '';
+if (isset($argv[1])) {
+    $settings_str = $argv[1];
+}
+if (mb_ereg_match('.*nocolumns',$settings_str)) {
+    $columns_mode = false;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,20 +53,24 @@ if (!(isset($tlp->{'P1'}))) {
         
     </head>
     <body>
-        <h1 class="englishtitle">Tractatus Logico-Philosophicus</h1>
-        <h1 class="germantitle">Logisch-philosophische Abhandlung</h1>
-        <h3 class="byline">By Ludwig Wittgenstein</h3>
-        <h3 class="pubinfo">First published by Kegan Paul (London), 1922.</h3>
-        <h3 class="pubinfo">Side-by-side-by-side edition, version <?php echo get_version_num(); ?> (<?php echo date('j F Y'); ?>), containing the original German, alongside both the Ogden/Ramsey, and Pears/McGuinness English translations.</h3>
-        <hr />
-        <h2 class="majordivision" id="tableofcontents">Contents</h2>
-        <ul class="contentslist">
-            <li class="contentsitem"><a href="#intro" class="contentslink">Introduction (by Bertrand Russell)</a></li>
-            <li class="contentsitem"><a href="#dedication" class="contentslink">Dedication page</a></li>
-            <li class="contentsitem"><a href="#preface" class="contentslink">Preface (and translations)</a></li>
-            <li class="contentsitem"><a href="#bodytext" class="contentslink">Tractatus Logico-Philosophicus (and translations)</a></li>
-            <li class="contentsitem"><a href="#index" class="contentslink">Index</a></li>
-        </ul>
+        <div id="coverpage">
+            <h1 class="englishtitle">Tractatus Logico-Philosophicus</h1>
+            <h1 class="germantitle">Logisch-philosophische Abhandlung</h1>
+            <h3 class="byline">By Ludwig Wittgenstein</h3>
+            <h3 class="pubinfo">First published by Kegan Paul (London), 1922.</h3>
+            <h3 class="pubinfo">Side-by-side-by-side edition, version <?php echo get_version_num(); ?> (<?php echo date('j F Y'); ?>), containing the original German, alongside both the Ogden/Ramsey, and Pears/McGuinness English translations.</h3>
+        </div>
+        <div id="contents">
+            <hr />
+            <h2 class="majordivision" id="tableofcontents">Contents</h2>
+            <ul class="contentslist">
+                <li class="contentsitem"><a href="#intro" class="contentslink">Introduction (by Bertrand Russell)</a></li>
+                <li class="contentsitem"><a href="#dedication" class="contentslink">Dedication page</a></li>
+                <li class="contentsitem"><a href="#preface" class="contentslink">Preface (and translations)</a></li>
+                <li class="contentsitem"><a href="#bodytext" class="contentslink">Tractatus Logico-Philosophicus (and translations)</a></li>
+                <li class="contentsitem"><a href="#index" class="contentslink">Index</a></li>
+            </ul>
+        </div>
         <hr />
         <div class="russellsintro">
             <h2 class="majordivision" id="intro">Introduction</h2>
@@ -64,6 +78,48 @@ if (!(isset($tlp->{'P1'}))) {
             
             <?php echo html_russells_intro(); ?>
             
+        </div>
+        <div id="dedicationpage">
+            <hr />
+            <h2 class="majordivision" id="dedication">Tractatus Logico-Philosophicus</h2>
+            <div class="dedicationtext">Dedicated<br />to the Memory of My Friend<br />David H. Pinsent<br /></div>
+            <div class="motto"><em class="germph">Motto:</em> &hellip; und alles, was man weiss, nicht bloss rauschen und brausen gehört hat, lässt sich in drei Worten sagen. –KÜRNBERGER.</div>
+        </div>
+        <div id="prefacesection">
+            <hr />
+            
+            <?php
+            
+            if ($columns_mode) {
+                columns_preface();
+            } else {
+            }
+            
+            ?>
+            
+        </div>
+        <div id="footnotes">
+            
+            
+            
+        </div>
+        <div id="bookcore">
+            
+            
+            
+        </div>
+        <div id="theindex">
+            
+            
+            
+        </div>
+        <div id="licenseinfo">
+            <hr />
+            <p class="licensep">
+                <span class="ccicongroup"><object data="images/pd.svg" type="image/svg+xml" class="ccicon"><img src="images/pd.png" alt="[PD]" class="ccicon" /></object></span> <span class="sflabel">Ludwig Wittgenstein’s <i>Tractatus Logico-Philosophicus</i> is in the Public Domain.</span></p>
+            <br clear="all"/>
+            <p class="licensep"><span class="ccicongroup"><object data="images/cc.svg" type="image/svg+xml" class="ccicon"><img src="images/cc.png" alt="[CC]" class="ccicon" /></object> <object data="images/by.svg" type="image/svg+xml" class="ccicon"><img src="images/by.png" alt="[BY]" class="ccicon" /></object> <object data="images/sa.svg" type="image/svg+xml" class="ccicon"><img src="images/sa.png" alt="[SA]" class="ccicon" /></object></span> <span class="sflabel">The layout of the side-by-side-by-side edition, including HTML mark-up and related content, by Kevin C. Klement, is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Creative Commons Attribution—Share Alike 3.0 United States License</a>.</span></p>
+            <p class="licensep">Latest version available at the project page: <a href="http://people.umass.edu/klement/tlp/">http://people.umass.edu/klement/tlp/</a></p>
         </div>
     </body>
 </html>
--- libhtmltlp.php
+++ libhtmltlp.php
@@ -13,6 +13,42 @@ function get_version_num() {
     return $vnum;
 }
 
+function columns_preface() {
+    global $tlp;
+    
+    echo '
+<h2 class="majordivision" id="preface">Vorwort (Preface)</h2>
+<table class="prefacetable">
+    <thead>
+        <tr class="prefheaderrow">
+            <th class="prefheadercell">German</th>
+            <th class="prefheadercell">Ogden</th>
+            <th class="prefheadercell">Pears/McGuinness</th>
+        </tr>
+    </thead>
+    <tbody>
+';
 
+    foreach ($tlp as $pn => $ptext) {
+        if (substr($pn,0,1) != 'P') {
+            break;
+        }
+        echo '<tr id="' . str_replace('P','pref',$pn) . '">' . PHP_EOL;
+        echo '<td class="gerpref">';
+        echo $ptext->German[0];
+        echo '</td>' . PHP_EOL;
+        echo '<td class="ogdpref">';
+        echo $ptext->Ogden[0];
+        echo '</td>' . PHP_EOL;
+        echo '<td class="pmcpref">';
+        echo $ptext->PearsMcGuinness[0];
+        echo '</td>' . PHP_EOL;
+        echo '</tr>' . PHP_EOL . PHP_EOL;
+    }
+    
+    echo '
+    </tbody>
+</table>';
+}
 
 ?>
